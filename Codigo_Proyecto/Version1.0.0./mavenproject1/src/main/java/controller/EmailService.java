83c527572b86295165d5318acb000da51a66ae2a
Julio Sánchez <106382109+Yesteb@users.noreply.github.com>
2026-05-12 12:32:25 -0500

    Delete Codigo_Proyecto/Version1.0.0. directory

--- Codigo_Proyecto/Version1.0.0./mavenproject1/src/main/java/controller/EmailService.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-public class EmailService {
-    public void enviarCodigo(String email, String codigo) {
-        System.out.println("Enviando código " + codigo + " a " + email);
-    }
-}
